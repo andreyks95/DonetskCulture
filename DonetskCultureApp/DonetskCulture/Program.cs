66ab1edc7bc16cd1fb600333574bb83726c669de
Andrey <andreyks95@gmail.com>
2018-03-22 16:14:43 +0200

    Взаимодействие с классом Connection как со статистическим

--- DonetskCultureApp/DonetskCulture/Program.cs
+++ DonetskCultureApp/DonetskCulture/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DonetskCulture
 {
@@ -14,9 +15,18 @@ namespace DonetskCulture
         [STAThread]
         static void Main()
         {
+            
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            //Создаём подкючение к БД
+            //Connection connection = new Connection();
+            Connection.TryConnectMySQL();
+            MySqlConnection connection = Connection.GetConnection;
+
+            //Запускаем форму
+            Application.Run(new EstablishmentsTableForm(connection));
         }
     }
 }
