57783863f8ecb8808ad076be87f9ceb53ca14874
Andrey <andreyks95@gmail.com>
2018-03-22 15:11:25 +0200

    Create class Connection. Который служит для подключения к базе данных MySQL

---
+++ DonetskCultureApp/DonetskCulture/Connection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DonetskCulture
+{
+    class Connection
+    {
+        private readonly MySqlConnection connection;
+
+        public Connection()
+        {
+            connection = new MySqlConnection("datasource=localhost; port=3306; username=root; password=KZ-_-S_D48Line");
+            OpenConnection();
+            TryConnectMySQL();
+        }
+
+        private void TryConnectMySQL()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Open connection!");
+                }
+                else
+                {
+                    MessageBox.Show("Fault connection!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OpenConnection() => connection.Open();
+        private void CloseConnection() => connection.Close();
+
+
+    }
+}
