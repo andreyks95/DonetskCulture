85f592da4e163bc940021482f558dd3bbceae28a
Andrey <andreyks95@gmail.com>
2018-06-25 16:07:13 +0300

    Add new form AddHeadEstablishmentForm

---
+++ DonetskCultureApp/DonetskCulture/Tables/AddRecordsTables/AddHeadEstablishmentForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace DonetskCulture.Tables.AddRecordsTables
+{
+    partial class AddHeadEstablishmentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CaptionLabel = new System.Windows.Forms.Label();
+            this.IDLabel = new System.Windows.Forms.Label();
+            this.IDTextBox = new System.Windows.Forms.TextBox();
+            this.fullNameLabel = new System.Windows.Forms.Label();
+            this.fullNameTextBox = new System.Windows.Forms.TextBox();
+            this.adminCorpsTextBox = new System.Windows.Forms.TextBox();
+            this.adminCorpsLlabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.mailLabel = new System.Windows.Forms.Label();
+            this.mailTextBox = new System.Windows.Forms.TextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.AddHeadEstablishmentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CaptionLabel
+            // 
+            this.CaptionLabel.AutoSize = true;
+            this.CaptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CaptionLabel.Location = new System.Drawing.Point(6, 9);
+            this.CaptionLabel.Name = "CaptionLabel";
+            this.CaptionLabel.Size = new System.Drawing.Size(296, 26);
+            this.CaptionLabel.TabIndex = 0;
+            this.CaptionLabel.Text = "Форма для додавання членів адміністративного\r\nкорпусу  закладу культури в базу да" +
+    "них";
+            this.CaptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.Location = new System.Drawing.Point(12, 63);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(50, 13);
+            this.IDLabel.TabIndex = 1;
+            this.IDLabel.Text = "ID члена";
+            // 
+            // IDTextBox
+            // 
+            this.IDTextBox.Location = new System.Drawing.Point(114, 63);
+            this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.Size = new System.Drawing.Size(180, 20);
+            this.IDTextBox.TabIndex = 2;
+            // 
+            // fullNameLabel
+            // 
+            this.fullNameLabel.AutoSize = true;
+            this.fullNameLabel.Location = new System.Drawing.Point(12, 104);
+            this.fullNameLabel.Name = "fullNameLabel";
+            this.fullNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.fullNameLabel.TabIndex = 3;
+            this.fullNameLabel.Text = "ПІБ члена";
+            // 
+            // fullNameTextBox
+            // 
+            this.fullNameTextBox.Location = new System.Drawing.Point(114, 104);
+            this.fullNameTextBox.Name = "fullNameTextBox";
+            this.fullNameTextBox.Size = new System.Drawing.Size(180, 20);
+            this.fullNameTextBox.TabIndex = 4;
+            // 
+            // adminCorpsTextBox
+            // 
+            this.adminCorpsTextBox.Location = new System.Drawing.Point(114, 147);
+            this.adminCorpsTextBox.Name = "adminCorpsTextBox";
+            this.adminCorpsTextBox.Size = new System.Drawing.Size(180, 20);
+            this.adminCorpsTextBox.TabIndex = 6;
+            // 
+            // adminCorpsLlabel
+            // 
+            this.adminCorpsLlabel.AutoSize = true;
+            this.adminCorpsLlabel.Location = new System.Drawing.Point(12, 141);
+            this.adminCorpsLlabel.Name = "adminCorpsLlabel";
+            this.adminCorpsLlabel.Size = new System.Drawing.Size(96, 26);
+            this.adminCorpsLlabel.TabIndex = 5;
+            this.adminCorpsLlabel.Text = "Адміністративний\r\nкорпус";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(12, 186);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(52, 13);
+            this.phoneLabel.TabIndex = 7;
+            this.phoneLabel.Text = "Телефон";
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(114, 186);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(180, 20);
+            this.phoneTextBox.TabIndex = 8;
+            // 
+            // mailLabel
+            // 
+            this.mailLabel.AutoSize = true;
+            this.mailLabel.Location = new System.Drawing.Point(12, 227);
+            this.mailLabel.Name = "mailLabel";
+            this.mailLabel.Size = new System.Drawing.Size(41, 13);
+            this.mailLabel.TabIndex = 9;
+            this.mailLabel.Text = "E - mail";
+            // 
+            // mailTextBox
+            // 
+            this.mailTextBox.Location = new System.Drawing.Point(114, 227);
+            this.mailTextBox.Name = "mailTextBox";
+            this.mailTextBox.Size = new System.Drawing.Size(180, 20);
+            this.mailTextBox.TabIndex = 10;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(120, 274);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(107, 23);
+            this.ClearButton.TabIndex = 21;
+            this.ClearButton.Text = "Очистити форму";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // AddHeadEstablishmentButton
+            // 
+            this.AddHeadEstablishmentButton.Location = new System.Drawing.Point(9, 274);
+            this.AddHeadEstablishmentButton.Name = "AddHeadEstablishmentButton";
+            this.AddHeadEstablishmentButton.Size = new System.Drawing.Size(96, 23);
+            this.AddHeadEstablishmentButton.TabIndex = 20;
+            this.AddHeadEstablishmentButton.Text = "Вставити запис";
+            this.AddHeadEstablishmentButton.UseVisualStyleBackColor = true;
+            this.AddHeadEstablishmentButton.Click += new System.EventHandler(this.AddHeadEstablishmentButton_Click);
+            // 
+            // AddHeadEstablishmentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 311);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.AddHeadEstablishmentButton);
+            this.Controls.Add(this.mailTextBox);
+            this.Controls.Add(this.mailLabel);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.adminCorpsTextBox);
+            this.Controls.Add(this.adminCorpsLlabel);
+            this.Controls.Add(this.fullNameTextBox);
+            this.Controls.Add(this.fullNameLabel);
+            this.Controls.Add(this.IDTextBox);
+            this.Controls.Add(this.IDLabel);
+            this.Controls.Add(this.CaptionLabel);
+            this.MaximumSize = new System.Drawing.Size(330, 350);
+            this.MinimumSize = new System.Drawing.Size(330, 350);
+            this.Name = "AddHeadEstablishmentForm";
+            this.Text = "AddHeadEstablishmentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CaptionLabel;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.TextBox IDTextBox;
+        private System.Windows.Forms.Label fullNameLabel;
+        private System.Windows.Forms.TextBox fullNameTextBox;
+        private System.Windows.Forms.TextBox adminCorpsTextBox;
+        private System.Windows.Forms.Label adminCorpsLlabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Label mailLabel;
+        private System.Windows.Forms.TextBox mailTextBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button AddHeadEstablishmentButton;
+    }
+}
---
+++ DonetskCultureApp/DonetskCulture/Tables/AddRecordsTables/AddHeadEstablishmentForm.cs
@@ -0,0 +1,70 @@
+﻿using DonetskCulture.Control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonetskCulture.Tables.AddRecordsTables
+{
+    public partial class AddHeadEstablishmentForm : Form
+    {
+
+
+        Command controlCommand; // = new Command();
+
+        //Класс для взаимодействия с классами
+        InteractingWithForms interactingWithForms = new InteractingWithForms();
+
+
+        string[] parametersTableSQL = new string[] { "@id_head", "@fullName", "@adminCorps", "@phone", "@email" };
+
+
+        public AddHeadEstablishmentForm()
+        {
+            InitializeComponent();
+        }
+
+        public AddHeadEstablishmentForm(Command command) : this()
+        {
+            controlCommand = command;
+        }
+
+        private string[] GetValuesFromForm()
+        {
+            //Вытаскиваем данные во время ввода
+            string idText = IDTextBox.Text;
+            string fullName = fullNameTextBox.Text;
+            string adminCorps = adminCorpsTextBox.Text;
+            string phone = phoneTextBox.Text;
+            string mail = mailTextBox.Text; 
+           
+            //Полученные значения с формы
+            return new string[] { idText, fullName, adminCorps, phone, mail };
+        }
+
+        private void AddHeadEstablishmentButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string queryInsert = "INSERT INTO donetsk_culture.heads_establishments " +
+                                         "VALUES(@id_head, @fullName, @adminCorps, @phone, @email)";
+                controlCommand.Insert(parametersTableSQL, GetValuesFromForm(), queryInsert);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n викликаний методом AddHeadEstablishmentButton_Click");
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            interactingWithForms.ClearForm(textBoxes: new[] { IDTextBox, fullNameTextBox, adminCorpsTextBox, phoneTextBox, mailTextBox });
+        }
+    }
+}
