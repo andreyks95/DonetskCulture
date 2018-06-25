1cbb6a82ead39ae477a96e14149ddebf15649e36
Andrey <andreyks95@gmail.com>
2018-06-25 12:15:18 +0300

    Add Form SelectHeadsOfEstablishmentsTableForm

--- DonetskCultureApp/DonetskCulture/Tables/SupportTables/SelectHeadsOfEstablishmentsTableForm.cs
+++ DonetskCultureApp/DonetskCulture/Tables/SupportTables/SelectHeadsOfEstablishmentsTableForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DonetskCulture.Control;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,41 @@ namespace DonetskCulture.Tables
 {
     public partial class SelectHeadsOfEstablishmentsTableForm : Form
     {
+        #region Поля класса
+
+        Command controlCommand = new Command();
+
+        string valueSelectedEstablishment = "";
+
+        //Дописать запросы
+        string partQueryBeforeWhere;
+        string partQueryAfterWhere;
+
+        BindingManagerBase bindingManagerBase;
+        DataTable table;
+
+        #endregion
+
         public SelectHeadsOfEstablishmentsTableForm()
         {
             InitializeComponent();
+        }
+
+        public SelectHeadsOfEstablishmentsTableForm(string value) : this()
+        {
+            valueSelectedEstablishment = value;
+        }
+
+        private void SelectHeadsOfEstablishmentsTableForm_Load(object sender, EventArgs e)
+        {
+            string query = partQueryBeforeWhere + " '" + valueSelectedEstablishment + "' " + partQueryAfterWhere;
+            table = controlCommand.FillDataGridView(dataGridView1, query);
+            bindingManagerBase = this.BindingContext[table];
+        }
+
+        private void AddHeadsOfEstablishmentButton_Click(object sender, EventArgs e)
+        {
+            //Передать конструктору ссылку на класс Command();
         }
     }
 }
