c019975d66c1f70ad71e11420636de6c6b7b8a11
Andrey <andreyks95@gmail.com>
2018-06-26 14:32:32 +0300

    Add fields, clearButton, declare new constructors

--- DonetskCultureApp/DonetskCulture/Tables/MainTables/CollectivesTableForm.cs
+++ DonetskCultureApp/DonetskCulture/Tables/MainTables/CollectivesTableForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DonetskCulture.Control;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,48 @@ namespace DonetskCulture.Tables
 {
     public partial class CollectivesTableForm : Form
     {
+
+        #region Поля класса
+        //Fields
+        //MySqlConnection connection;//= Connection.GetConnection; //Избыточное
+        Command controlCommand = new Command();
+
+        //Класс для взаимодействия с классами
+        InteractingWithForms interactingWithForms = new InteractingWithForms();
+
+        string otherQuery = " select collectives.id_collective AS 'ID колективу', collectives.name AS 'Назва', collectives.form AS 'Форма', " +
+                            " collectives.genre AS 'Жанр', collectives.ageCategory AS 'Вікова категорія', collectives.rank AS 'Звання' " +
+                            " from donetsk_culture.collectives GROUP BY(name); ";
+
+        string partSelectSearchQueryWithGroup = "";
+
+
+        string partSelectSearchQuery = "";
+
+
+        string[] parametersTableSQL = new string[] { "@id_collective", "@name", "@form", "@genre", "@ageCategory", "@rank", "@id_manager" };
+
+        BindingManagerBase bindingManagerBase;
+        DataTable table;
+
+        #endregion
+
+
         public CollectivesTableForm()
         {
             InitializeComponent();
+        }
+
+        public CollectivesTableForm(Command command) : this()
+        {
+            controlCommand = command;
+        }
+
+
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            interactingWithForms.ClearForm(new[] { ageCategoryComboBox, rankComboBox, managerComboBox }, new[] { IDTextBox, nameTextBox, formTextBox, genreTextBox });
         }
     }
 }
